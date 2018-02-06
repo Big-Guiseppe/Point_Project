61ef378947d5d3347a20dab8f29c28ca93c924f7
Guisepepe <Guiseppe@github.com>
2018-02-06 08:06:25 -0500

    Initial commit

---
+++ PointMainLessHelp.java
@@ -0,0 +1,25 @@
+// A program that deals with 2D points.
+// Fifth version, to accompany immutable Point class.
+
+public class PointMainLessHelp {
+    public static void main(String[] args) {
+        // create two Point objects
+        Point p1 = new Point(?);
+        Point p2 = new Point(?);
+
+        // print each point and its distance from origin
+        System.out.println("p1 is " + ?);
+        System.out.println("p1 distance from origin = " + ?);
+
+        System.out.println("p2 is " + ?);
+        System.out.println("p2 distance from origin = " + ?);
+
+        // translate each point to a new location
+        p1 = p1.translate(?);
+        p2 = p2.translate(?);
+
+        // print the points again
+        System.out.println("p1 translated is now " + ?);
+        System.out.println("p2 translated is now " + ?);
+    }
+}
